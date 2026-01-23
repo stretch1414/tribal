9222b43b0433e2f82c644d36608ae72edfab9940
stretch1414 <jmoser1414@gmail.com>
2026-01-22 22:57:18 -0500

    Rename prisma folder to db

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,10 @@ import path from 'node:path';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-	schema: path.join('prisma', 'schema.prisma'),
+	schema: path.join('db', 'schema.prisma'),
 	migrations: {
 		path: path.join('db', 'migrations'),
-		seed: 'bun prisma/seed.ts',
+		seed: 'bun db/seed.ts',
 	},
 	datasource: {
 		url: env('DATABASE_URL'),
